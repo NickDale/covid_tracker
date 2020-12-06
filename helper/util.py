96b6100811cb4c3a8c219db4249ea7237a7b30c5
Balogh Norbert <balogh.norbert92@gmail.com>
2020-12-06 19:06:03 +0100

    little refactor

--- helper/util.py
+++ helper/util.py
@@ -19,12 +19,3 @@ def create_popup(html, data):
 
 def read_pop_html():
     return open(POPUP_HTML_URL, 'r').read()
-
-
-# not used
-def create_popup_usa(html, data):
-    formatted_popup = html % (data.Country_Region, 'N/A', 'N/A', data.Deaths, 'N/A', data.Confirmed,
-                              data.Recovered, 'N/A', data.Case_Fatality_Ratio, 'N/A', data.Incident_Rate
-                              )
-    iframe = branca.element.IFrame(html=formatted_popup, width=500, height=300)
-    return folium.Popup(iframe, max_width=350)
